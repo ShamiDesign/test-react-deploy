3de587e7d69e3099cacd32b76b26fbcfd4763c80
ShamiDesign <shami.design32@gmail.com>
2025-08-25 13:59:31 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/test-deploy"
+  base:process.env.VITE-BASE-BASE || "/test-react-deploy"
 })
